6ac5b562b4a0028b7cf1b616559d7e519541757e
hector1234567 <hectorbg303@gmail.com>
2022-11-18 00:16:01 +0100

    Red border

--- js/validator.js
+++ js/validator.js
@@ -31,14 +31,17 @@ export class Validator {
 
     showErrorMessage(message, field) {
         const fieldContainer = this.form.querySelector(`.field-container--${field}`);
+        fieldContainer.querySelectorAll('input[name]').forEach(input => input.classList.add('border-red'));
         fieldContainer.insertAdjacentHTML('beforeend', `<span class="error-message">${message}</span>`);
     }
 
     clearErrorMessage(field) {
+        this.form.querySelectorAll(`.field-container--${field} input[name]`).forEach(input => input.classList.remove('border-red'));
         this.form.querySelector(`.field-container--${field} .error-message`)?.remove();
     }
 
     clearErrorMessages() {
+        this.form.querySelectorAll(`input[name]`).forEach(input => input.classList.remove('border-red'));
         this.form.querySelectorAll(`.error-message`)?.forEach(err => err.remove());
     }
 }
